89bce026b01874676395e905a428652a54fe9f5d
KalabSisaySE <kyimusic11@gmail.com>
2023-01-15 15:13:22 +0300

    complete task-4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+-- creates a trigger that decreases the quantity of an item
+-- after adding a new order
+
+USE holberton;
+
+CREATE TRIGGER `items_INS`
+AFTER INSERT
+ON `orders` FOR EACH ROW
+-- update the items table after each order decrease the quantity 
+-- by the number of orders
+UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
