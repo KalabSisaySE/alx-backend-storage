19ccea2c0a3754cb424660d78ef982a05ba86c9f
KalabSisaySE <kyimusic11@gmail.com>
2023-01-18 22:37:48 +0300

    fix task-5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,25 +6,14 @@ import redis
 import requests
 
 
-class Cache:
-    """uses redis to cache data"""
+def get_page(self, url: str) -> str:
+    """returns the content of the `url`
+    tracks the number of times a particular url was accessed
+    """
+    response = requests.get(url)
+    if redis.Redis.get(name=url):
+        redis.Redis.incr(url, 1)
+    else:
+        redis.Redis.set(url, 1, 10)
 
-    def __init__(self) -> None:
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    def get_page(self, url: str) -> str:
-        """returns the content of the `url`
-        tracks the number of times a particular url was accessed
-        """
-        response = requests.get(url)
-        if self._redis.get(name=url):
-            self._redis.incr(url, 1)
-        else:
-            self._redis.set(url, 1, 10)
-
-        return response.text
-
-    def count_access(self, url):
-        """returns the number of access for a particular `url`"""
-        return self._redis.get(url)
+    return response.text
